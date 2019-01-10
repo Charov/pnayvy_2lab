97cbae82b79dedabf4ecfe480f710c5b5cbf5eb1
Charov <artyr13531@mail.ru>
2019-01-11 00:39:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyString c1 = new MyString("Hello");
+            MyString c2 = new MyString("World");
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+
+            Console.ReadKey();
+        }
+    }
+}
